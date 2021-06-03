e5f6da721c77224b704f861de50a656b72bd47e7
susmitmishra125 <susmitmishra125@gmail.com>
2021-06-03 21:55:25 +0530

    Training completed and working with dev acc 0.73

--- main.py
+++ main.py
@@ -12,43 +12,16 @@ import shutil
 # os.chdir("/home/mtech1/19CS60R28/susmit/DocRed_hongwang600/DocRed")
 
 from nltk.tokenize import WordPunctTokenizer
-from pytorch_transformers import *
+from pytorch_transformers import BertModel, BertTokenizer, BertPreTrainedModel, AdamW
 import torch
-from torch.utils.data import Dataset, DataLoader
 import torch.autograd as autograd
 from torch import nn, optim
 import torch.nn.functional as F
 from tqdm import tqdm
 sys.path.append("/home/mtech1/19CS60R28/susmit/DocRed_hongwang600/DocRed/models")
-from bert import Bert
-
-# os.chdir("/home/mtech1/19CS60R28/susmit/DocRed_hongwang600")
-RANDOM_SEED = 42
-in_path='data'
-out_path='prepro_data'
-os.environ['CUDA_VISIBLE_DEVICES']="0,1"
-PRE_TRAINED_MODEL_NAME='bert-base-uncased'
-save_model_file = os.path.join('checkpoint', 'model.h5py')
-model_name='bert'
-BATCH_SIZE=4# for training
-BATCH_SIZE_PRED=4
-EPOCH = 100
-update_freq = 4
-early_stop_count = 5
-
-n_gpu = torch.cuda.device_count()
-if not os.path.exists(out_path):
-    os.mkdir(out_path)
-
-MAX_LEN=512
-SEP='[SEP]'
-MASK = '[MASK]'
-CLS = "[CLS]"
-bert = Bert(BertModel, PRE_TRAINED_MODEL_NAME)
-
-train_annotated_file_name = os.path.join(in_path, 'train_annotated.json')
-dev_file_name = os.path.join(in_path, 'dev.json')
-test_file_name = os.path.join(in_path, 'test.json')
+# from bert import Bert
+# from torchviz import make_dot
+torch.backends.cudnn.deterministic = True
 
 def logging(*msg):
     for i in range(0,len(msg)):
@@ -60,6 +33,8 @@ def logging(*msg):
         with open(os.path.join(os.path.join("log", model_name+'.txt')), 'a+') as f_log:
             f_log.write(str(msg[i])+end)
             f_log.close()
+
+
 def format_time(elapsed):
     '''
     Takes a time in seconds and returns a string hh:mm:ss
@@ -69,12 +44,14 @@ def format_time(elapsed):
     
     # Format as hh:mm:ss
     return str(datetime.timedelta(seconds=elapsed_rounded))
+
+
 def set_random_seeds(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-set_random_seeds(RANDOM_SEED)
+
 
 def load_ckp(checkpoint_fpath, model, optimizer):
     try:
@@ -85,6 +62,7 @@ def load_ckp(checkpoint_fpath, model, optimizer):
     optimizer.load_state_dict(checkpoint['optimizer'])
     return checkpoint['best_epoch_idx'], checkpoint['best_epoch_seed'], checkpoint['best_dev_acc'], checkpoint['epoch'], model, optimizer
 
+
 def save_ckp(state, is_best, checkpoint_dir, best_model_dir):
     f_path = checkpoint_dir+'/checkpoint.pt'
     torch.save(state, f_path)
@@ -92,14 +70,11 @@ def save_ckp(state, is_best, checkpoint_dir, best_model_dir):
             best_fpath = best_model_dir+'/best_model.pt'
             shutil.copyfile(f_path, best_fpath)
 
-
-
 def preprocess(data_file_name, max_length = 512, is_training = True, suffix=''):
     """
     inserts unused tokens and saves them in a dict
     """
     ori_data=json.load(open(data_file_name))[0:10]
-    max_sent_count=0#maximum number of sentences in a doc across the dataset
     input_sent = []
     labels=[]
     i=0
@@ -108,7 +83,6 @@ def preprocess(data_file_name, max_length = 512, is_training = True, suffix=''):
         # sys.stdout.write("\r%d/%d docs"%(i,len(ori_data)))
         # this dict is used to take care of multiple relations with same head and tail
         head_tail_index={}
-        max_sent_count=max(max_sent_count,len(doc['sents']))
         for label in doc['labels']:
             idx_list=[]
             head=doc['vertexSet'][label['h']]
@@ -135,6 +109,7 @@ def preprocess(data_file_name, max_length = 512, is_training = True, suffix=''):
 
             input_sent.append(temp_doc['sents'])
             labels.append(label['evidence'])
+            
     data={}
     data['input_sent']=input_sent
     data['labels']=labels
@@ -145,27 +120,34 @@ def preprocess(data_file_name, max_length = 512, is_training = True, suffix=''):
     out_file.close()
     logging("completed saving\n")
 
-preprocess(train_annotated_file_name, max_length = 512, is_training = False, suffix='train')
-preprocess(dev_file_name, max_length = 512, is_training = False, suffix='dev')
-
-
-
-train_data = json.load(open(os.path.join(out_path,'train_data.json'),'r'))
-dev_data = json.load(open(os.path.join(out_path,'dev_data.json'),'r'))
-
 
 
 def get_max(cur_samples):
     max_doc_len = 0
     max_sent_count = 0
+    batch_sent_ids = []
+    i=0
+    sent_attention = []
     for doc in cur_samples:
-        s=0
+        s=2 # because CLS and SEP token will be added
+        doc_sent_ids = []
         for sent in doc:
-            s += len(sent)
+            sent_ids = []
+            for token in sent:
+                if token.startswith('[unused'):
+                    sent_ids += [int(token[-2])+1]
+                else:
+                    sent_ids+=tz.convert_tokens_to_ids(tz.tokenize(token))
+            s += len(sent_ids)
+            doc_sent_ids.append(sent_ids)
+        batch_sent_ids.append(doc_sent_ids)
+        s=min(s,512)
         max_doc_len = max(max_doc_len,s)
         max_sent_count = max(max_sent_count,len(doc))
+        i+=1
+    return max_doc_len, max_sent_count, batch_sent_ids
 
-    return max_doc_len,max_sent_count
+
 def get_batch_data(cur_samples_doc,cur_samples_evi = None):
     """
     input is a list of docs
@@ -174,36 +156,37 @@ def get_batch_data(cur_samples_doc,cur_samples_evi = None):
     Returns dictionary of training samples and labels as numpy array
     """
     no_samples = len(cur_samples_doc)
-    max_doc_len,max_sent_count = get_max(cur_samples_doc)
-    max_doc_len+=2 # because we are going to add tokens
-    max_doc_len = min(MAX_LEN,max_doc_len)
+    max_doc_len,max_sent_count, batch_sent_ids= get_max(cur_samples_doc)
     input_ids = np.zeros((no_samples,max_doc_len))
     sent_attention = np.zeros((no_samples,max_doc_len))
     sent_mask = np.zeros((no_samples,max_sent_count, max_doc_len))# required for averaging the sentence embeddings
     evi_target = np.zeros((no_samples,max_sent_count))
     for i in range(no_samples):
-        lower_index = 1# 1 because CLS token is to be appended
-        doc=[]
+        doc=[101] # CLS token
+        lower_index = 1
         j=0
-        for sent in cur_samples_doc[i]:
-            if(lower_index>=510):
+        for sent in batch_sent_ids[i]:
+            if(lower_index>510):
                 break
             doc+=sent
-            sent_mask[i][j][lower_index:min(lower_index+len(sent),510)]=1.0
+            sent_mask[i][j][lower_index:min(lower_index+len(sent),511)]=1.0
             lower_index+=len(sent)
-            lower_index=min(lower_index,510)
-            if(lower_index==510):
-                doc=doc[:510]
+            if(lower_index>510):
+                doc=doc[:511]
             j+=1
+        doc.append(102) # SEP token
         if j<max_sent_count:
-            sent_mask[i][j:,:]=1.0
-        sent_ids,attention,start_ids = bert.subword_tokenize_to_ids(doc,max_doc_len)
-        input_ids[i],sent_attention[i]=sent_ids[0],attention[0]
+            sent_mask[i][j:,:]=1.0 # for pad sentences
+        # sent_ids,attention,start_ids = bert.subword_tokenize_to_ids(doc,max_doc_len)
+        # input_ids[i],sent_attention[i]=sent_ids[0],attention[0]
+        for j in range(len(doc)):
+            input_ids[i][j]=doc[j]
+        sent_attention[i][:len(doc)] = 1
         # _,_,_ = bert.subword_tokenize_to_ids(doc,max_doc_len+2)
         if cur_samples_evi != None :
             evi_target[i][cur_samples_evi[i]]=1
             if j<max_sent_count:
-                evi_target[i][j:]=2
+                evi_target[i][j:]=2 # this will be used later to remove loss from this sample
     return {
         'sent_ids':input_ids,
         'sent_attention':sent_attention,
@@ -212,23 +195,29 @@ def get_batch_data(cur_samples_doc,cur_samples_evi = None):
     }
 
 
-
 class EvidenceClassifier(nn.Module):
     def __init__(self):
         super(EvidenceClassifier, self).__init__()
         self.bert = BertModel.from_pretrained(PRE_TRAINED_MODEL_NAME)
-        self.dense = nn.Linear(self.bert.config.hidden_size, 2)
-        self.logsoftmax = nn.LogSoftmax(dim=-1)
-        self.softmax = nn.Softmax(dim=-1)
+        self.dense = nn.Linear(self.bert.config.hidden_size, 1)
+        self.sigmoid = nn.Sigmoid()
+        # self.logsoftmax = nn.LogSoftmax(dim=-1)
+        # self.softmax = nn.Softmax(dim=-1)
     def forward(self, input_ids, attention_mask,sent_mask,is_training=False):
         last_hidden_state, pooled_output = self.bert(input_ids=input_ids,attention_mask=attention_mask)
-        sent_mask=sent_mask.float()
+        # sent_mask=sent_mask.float()
         output=torch.bmm(sent_mask,last_hidden_state)/sent_mask.sum(axis=2)[...,None]
-        logits = self.dense(output)
+        logits = self.dense(output) # shape b,k,1
+        logits = logits.view((output.shape[0],output.shape[1]))
+        # logits = logits.squeeze() # shape b,k
+        # note the sigmod layer will be inside the BCEWithLogitsLoss
         if is_training:
-            return self.logsoftmax(logits)
+            return logits
         else:
-            return self.softmax(logits)
+            return self.sigmoid(logits)
+        #     return self.logsoftmax(logits)
+        # else:
+        #     return self.softmax(logits)
 
 def train(train_data,dev_data,model_file):
     input_sent = train_data['input_sent']
@@ -239,15 +228,18 @@ def train(train_data,dev_data,model_file):
     batch_size=BATCH_SIZE
     batch_count=int(math.ceil(train_size)/batch_size)
     model=EvidenceClassifier()
-    weights = torch.tensor([1.0,10.0])
+    weights = torch.tensor([1.0])
     
-    logging(model)
+    # logging(model)
     
     if torch.cuda.is_available():
-        model.cuda
-        model = torch.nn.DataParallel(model)
+        # model.cuda
+        model = nn.DataParallel(model, device_ids = [0,1])
+        model.to(f'cuda:{model.device_ids[0]}')
+
         weights = weights.cuda()
-    criterion = nn.NLLLoss(weight =weights, reduction='mean',ignore_index=2)
+    # criterion = nn.NLLLoss(weight=weights, reduction='mean',ignore_index=2)
+    criterion = nn.BCEWithLogitsLoss(reduction='mean',pos_weight= weights)
     optimizer = AdamW(model.parameters(),lr=1e-05,correct_bias=False)
     
     logging(optimizer)
@@ -261,11 +253,11 @@ def train(train_data,dev_data,model_file):
     
     for epoch_idx in range(start_epoch, EPOCH):
         model.train()
-        # model.zero_grad()
+        model.zero_grad()
         logging('Epoch:', epoch_idx + 1)
         cur_seed = RANDOM_SEED + epoch_idx + 1
         set_random_seeds(cur_seed)
-        
+        random.shuffle(cur_shuffled_train_data)
         start_time = datetime.datetime.now()
         train_loss_val = 0
         is_best = False
@@ -276,7 +268,7 @@ def train(train_data,dev_data,model_file):
             data = get_batch_data(input_sent[batch_start:batch_end],target[batch_start:batch_end])
             batch_sent_ids = torch.tensor(data['sent_ids']).to(torch.int64)
             batch_sent_attention = torch.tensor(data['sent_attention']).to(torch.int64)
-            batch_sent_mask = torch.tensor(data['sent_mask']).to(torch.int64)
+            batch_sent_mask = torch.tensor(data['sent_mask']).float()
             batch_evi_targets = torch.tensor(data['targets']).to(torch.int64)
             
             if torch.cuda.is_available():
@@ -288,22 +280,26 @@ def train(train_data,dev_data,model_file):
             batch_sent_attention = autograd.Variable(batch_sent_attention)
             batch_sent_mask = autograd.Variable(batch_sent_mask)
             batch_evi_targets = autograd.Variable(batch_evi_targets)
-            
-            outputs = model(batch_sent_ids,batch_sent_ids,batch_sent_mask,is_training=True)
-            y_pred = outputs.reshape((outputs.shape[0]*outputs.shape[1],outputs.shape[2])).cuda()
-            labels = batch_evi_targets.reshape((batch_evi_targets.shape[0]*batch_evi_targets.shape[1])).cuda()
+
+            outputs = model(batch_sent_ids,batch_sent_attention,batch_sent_mask,is_training=True)
+            y_pred = outputs.view((outputs.shape[0]*outputs.shape[1]))
+            labels = batch_evi_targets.view((batch_evi_targets.shape[0]*batch_evi_targets.shape[1])).float()
+            y_pred[labels==2] = -np.Inf
+            labels[labels==2] = 0
+
             loss = criterion(y_pred,labels)
             loss.backward()
             torch.nn.utils.clip_grad_norm_(model.parameters(), 10.0)
-            if (batch_idx + 1) % update_freq == 0:
-                optimizer.step()
-                model.zero_grad()
-            train_loss_val+=loss.item()
-        train_loss_val/=batch_count
+            # if (batch_idx + 1) % update_freq == 0:
+            optimizer.step()
+            # model.zero_grad()
+            train_loss_val +=loss.item()
+        train_loss_val /=batch_count
         end_time = datetime.datetime.now()
-        logging('Training_loss: ',train_loss_val)
+        logging('\nTraining_loss: ',train_loss_val)
         logging('Time: ',end_time-start_time)
         logging('\nDev_Results\n')
+
         acc,F1 = predict(dev_data,model)
         logging('Dev acc:',round(acc,3))
         logging('Dev F1:',round(F1,3))
@@ -322,14 +318,17 @@ def train(train_data,dev_data,model_file):
         'state_dict': model.state_dict(),
         'optimizer': optimizer.state_dict(),
         }
-#         save_ckp(checkpoint, is_best, 'checkpoint', 'checkpoint') # uncomment this when running for long time
-        logging('checkpoint updated\n\n')
+        save_ckp(checkpoint, is_best, 'checkpoint', 'checkpoint') # uncomment this when running for long time
+        logging('\ncheckpoint updated\n\n')
         if epoch_idx+1-best_epoch_idx>=early_stop_count:
             break
 
     logging("*"*50)
     logging('Best epoch',best_epoch_idx)
     logging('Best Epoch Seed:', best_epoch_seed)
+
+def test
+
 
 
 def predict(dev_data,model,threshold=0.5):
@@ -353,25 +352,25 @@ def predict(dev_data,model,threshold=0.5):
 
         batch_sent_ids = torch.tensor(data['sent_ids']).to(torch.int64)
         batch_sent_attention = torch.tensor(data['sent_attention']).to(torch.int64)
-        batch_sent_mask = torch.tensor(data['sent_mask']).to(torch.int64)
+        batch_sent_mask = torch.tensor(data['sent_mask']).float()
         batch_evi_targets = torch.tensor(data['targets']).to(torch.int64)
         
         if torch.cuda.is_available():
             batch_sent_ids = batch_sent_ids.cuda()
-            batch_sent_ids = batch_sent_attention.cuda()
+            batch_sent_attention = batch_sent_attention.cuda()
             batch_sent_mask = batch_sent_mask.cuda()
             batch_evi_targets = batch_evi_targets.cuda()
         with torch.no_grad():
-            outputs = model(batch_sent_ids,batch_sent_ids,batch_sent_mask,is_training=False)
+            outputs = model(batch_sent_ids,batch_sent_attention,batch_sent_mask,is_training=False)
         for i in range(outputs.shape[0]):
             for j in range(outputs.shape[1]):
                 if(batch_evi_targets[i][j]==2):
                     continue
-                if outputs[i][j][1]>=threshold and batch_evi_targets[i][j]==1:
+                if outputs[i][j]>=threshold and batch_evi_targets[i][j]==1:
                     true_positive+=1
-                elif outputs[i][j][1]>=threshold and batch_evi_targets[i][j]==0:
+                elif outputs[i][j]>=threshold and batch_evi_targets[i][j]==0:
                     false_positive+=1
-                elif outputs[i][j][1]<=threshold and batch_evi_targets[i][j]==1:
+                elif outputs[i][j]<=threshold and batch_evi_targets[i][j]==1:
                     false_negative+=1
                 else:
                     true_negative+=1
@@ -384,6 +383,49 @@ def predict(dev_data,model,threshold=0.5):
     recall = true_positive/(true_positive+false_negative+1e-05)
     F1 = 2*recall*preciscion/(preciscion+recall+1e-05)
     acc = true_positive+true_negative
+    model.train()
     return acc,F1
 
-train(train_data = train_data,dev_data = dev_data, model_file=save_model_file)
+
+
+if __name__ == "__main__":
+    RANDOM_SEED = 42
+    set_random_seeds(RANDOM_SEED)
+
+    in_path='data'
+    out_path='prepro_data'
+    os.environ['CUDA_VISIBLE_DEVICES']="0,1"
+    PRE_TRAINED_MODEL_NAME='bert-base-uncased'
+    save_model_file = os.path.join('checkpoint', 'model.h5py')
+    model_name='bert'
+    BATCH_SIZE=16# for training
+    BATCH_SIZE_PRED=64
+    EPOCH = 100
+    update_freq = 2
+    early_stop_count = 5
+
+    n_gpu = torch.cuda.device_count()
+    if not os.path.exists(out_path):
+        os.mkdir(out_path)
+
+    MAX_LEN=512
+    SEP='[SEP]'
+    MASK = '[MASK]'
+    CLS = "[CLS]"
+    # bert = Bert(BertModel, PRE_TRAINED_MODEL_NAME)
+    tz = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME) # tokenizer
+    train_annotated_file_name = os.path.join(in_path, 'train_annotated.json')
+    dev_file_name = os.path.join(in_path, 'dev.json') # but his will be used for testing purposes
+    # test_file_name = os.path.join(in_path, 'test.json') # not used because labels are not available
+    # preprocessing
+    preprocess(train_annotated_file_name, max_length = 512, is_training = True, suffix='train')
+    preprocess(dev_file_name, max_length = 512, is_training = False, suffix='test') # will use this as test data
+
+
+    train_data = json.load(open(os.path.join(out_path,'train_data.json'),'r'))
+
+    test_data = json.load(open(os.path.join(out_path,'test_data.json'),'r'))
+
+    # training
+    train(train_data = train_data,dev_data = dev_data, model_file=save_model_file)
+
